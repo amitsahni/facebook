e502e57225445223a23514cb4903071873d377f8
amit <amit@clickapps.co>
2019-08-11 19:22:24 +0530

    updated to Androidx

--- app/src/main/java/com/fb/MainActivity.kt
+++ app/src/main/java/com/fb/MainActivity.kt
@@ -2,7 +2,7 @@ package com.fb
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.widget.TextView
 import com.auth.facebook.FacebookManager
